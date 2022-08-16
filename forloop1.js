d226deda71059ca009b39acdf7eb5991fdca71a5
DivyaMulukuri123 <divya.mulukuri586@gmail.com>
2022-08-16 18:13:22 +0200

    commit forloop

---
+++ forloop1.js
@@ -0,0 +1,44 @@
+/*for(let i=10;i>0;i--)
+{
+    console.log(i);
+}*/
+/*for(i=2;i<=10;i+=2)
+{
+    if(i==8)
+    break;
+    console.log(i);
+}*/
+for(i=2;i<=10;i+=2)
+{
+    if(i==4||i==6)+,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    break;
+    console.log(i);
+}
