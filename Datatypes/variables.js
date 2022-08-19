1753ca0dbd210561e41e94400f8536af4f125f2a
DivyaMulukuri123 <divya.mulukuri586@gmail.com>
2022-08-19 10:36:30 +0200

    commit

--- Datatypes/variables.js
+++ Datatypes/variables.js
@@ -1,7 +1,11 @@
-var i=20
+/*var i=20
 var j=30
 var sum = i+j
 console.log(sum)
-/*var sentence = 'Hallo divya'
+var sentence = 'Hallo divya'
 var sentence1 = 'i am ur friend'
 console.log(sentence + sentence1)*/
+var names ={
+    name1: 'Hallo',
+    name2:'Prashanth'
+}
